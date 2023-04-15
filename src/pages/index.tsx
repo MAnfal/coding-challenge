9a2e453ade56343ea4e302de2106fd8637e0ecf7
Anfal <muhammadanfal0@gmail.com>
2023-04-15 15:59:44 -0700

    Remove console log

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,8 +35,6 @@ export default function Home() {
 
         const { data } = await response.json();
 
-        console.log(data);
-
         toast.dismiss();
 
         if (data.Response === 'False' || !data.Search) {
